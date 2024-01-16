906856c0b77bd66a72784bbf5921153f61c1332e
Aymen Ben Nasr <aymen.bennaser12@gmail.com>
2024-01-16 08:46:23 +0100

    initial microservice structure

---
+++ src/test/java/com/risque/retailservice/RetailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.risque.retailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
